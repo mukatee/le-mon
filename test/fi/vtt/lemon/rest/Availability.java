0d39889a33438bf50c1083a4890d8eafcc1f9fad
tkanstren <tkanstren@localhost>
2012-09-17 23:38:46 -0400

    minor test updates

--- test/fi/vtt/lemon/rest/Availability.java
+++ test/fi/vtt/lemon/rest/Availability.java
@@ -5,18 +5,24 @@ import com.sun.jersey.api.client.WebResource;
 import fi.vtt.lemon.RabbitConst;
 import fi.vtt.lemon.probes.tester.TestProbe;
 import fi.vtt.lemon.probes.tester.TestProbe1;
+import fi.vtt.lemon.probes.tester.TestProbe2;
+import fi.vtt.lemon.probes.tester.TestProbe3;
 import fi.vtt.lemon.server.LemonServer;
 import fi.vtt.lemon.server.external.JerseyApp;
 import fi.vtt.lemon.server.external.RESTConst;
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONObject;
+import org.junit.Test;
 import osmo.common.log.Logger;
+
+import static org.junit.Assert.*;
 
 import javax.ws.rs.core.MediaType;
 
 /** @author Teemu Kanstren */
 public class Availability {
-  public static void main(String[] args) throws Exception {
+  @Test
+  public void oneProbe() throws Exception {
 //    Logger.debug = true;
     LemonServer.main(null);
 
@@ -25,11 +31,41 @@ public class Availability {
 
     Client client = Client.create();
     WebResource wr = client.resource("http://localhost:11111/rest/");
-    Thread.sleep(5555);
+    Thread.sleep(2222);
     String data = wr.path(RESTConst.PATH_AVAILABILITY).accept(MediaType.APPLICATION_JSON).get(String.class);
     JSONObject json = new JSONObject(data);
     JSONArray array = json.getJSONArray("availability");
+    assertEquals("Availability size:"+array.toString(), 1, array.length());
     JSONObject item = array.getJSONObject(0);
-    System.out.println("json:"+item.get(RabbitConst.PARAM_MEASURE_URI));
+    assertEquals("Registered probe", "MFW://Firewall/Bob1/Configuration file/Bobby1", item.getString(RabbitConst.PARAM_MEASURE_URI));
+  }
+
+  @Test
+  public void threeProbes() throws Exception {
+//    Logger.debug = true;
+    LemonServer.main(null);
+
+    TestProbe probe1 = new TestProbe1();
+    probe1.start();
+
+    TestProbe probe2 = new TestProbe2();
+    probe2.start();
+
+    TestProbe probe3 = new TestProbe3();
+    probe3.start();
+
+    Client client = Client.create();
+    WebResource wr = client.resource("http://localhost:11111/rest/");
+    Thread.sleep(2222);
+    String data = wr.path(RESTConst.PATH_AVAILABILITY).accept(MediaType.APPLICATION_JSON).get(String.class);
+    JSONObject json = new JSONObject(data);
+    JSONArray array = json.getJSONArray("availability");
+    assertEquals("Availability size:"+array.toString(), 3, array.length());
+    JSONObject item1 = array.getJSONObject(0);
+    JSONObject item2 = array.getJSONObject(1);
+    JSONObject item3 = array.getJSONObject(2);
+    assertEquals("Registered probe 1", "MFW://Communication protocol/Bob3/Encryption key length/Bobby3", item1.getString(RabbitConst.PARAM_MEASURE_URI));
+    assertEquals("Registered probe 2", "MFW://Firewall/Bob1/Configuration file/Bobby1", item2.getString(RabbitConst.PARAM_MEASURE_URI));
+    assertEquals("Registered probe 3", "MFW://Spam Filter/Bob2/configuration file/Bobby", item3.getString(RabbitConst.PARAM_MEASURE_URI));
   }
 }
